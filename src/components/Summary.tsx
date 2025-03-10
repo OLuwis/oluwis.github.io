e815e063524df76452d4bd093706f9b690454481
Luis Miguel <oluismrs@gmail.com>
2025-03-10 14:15:40 -0300

    Update Summary.tsx

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -14,7 +14,7 @@ export function Summary({ summary, className }: AboutProps) {
   return (
     <Section className={className}>
       <h2 className="text-xl font-bold" id="about-section">
-        Resumo
+        Introdução
       </h2>
       <div
         className="text-pretty font-mono text-sm text-foreground/80 print:text-[12px]"
